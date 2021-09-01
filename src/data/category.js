2ce52672c3246a55f74e779c9d9436421f63af1c
Pratik667 <44764336+Pratik667@users.noreply.github.com>
2021-09-01 22:33:06 +0530

    category

--- src/data/category.js
+++ src/data/category.js
@@ -19,17 +19,45 @@ return(
                 <div className="row">                      
                
                     <div className="col"> 
-                        <div className="card" >
-                            <img src="..." className="card-img-top" alt="..."></img>
+                        <div className="card category-card" >
+                            <img src="https://raw.githubusercontent.com/Pratik667/myshop/development/src/assets/jwellery.webp" className="card-img-top" alt="..."></img>
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h3 className="card-title">Jwellery</h3>                               
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                                                     
                     </div> 
-                                
+                    <div className="col"> 
+                        <div className="card category-card" >
+                            <img src="https://raw.githubusercontent.com/Pratik667/myshop/development/src/assets/electronics.jpg" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Electronics</h5>                               
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                                                    
+                    </div> 
+                     <div className="col"> 
+                        <div className="card category-card" >
+                            <img src="https://raw.githubusercontent.com/Pratik667/myshop/development/src/assets/mensclothing.jpeg" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Men's Wear</h5>                               
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                                                    
+                    </div> 
+                    <div className="col"> 
+                        <div className="card category-card" >
+                            <img src="https://raw.githubusercontent.com/Pratik667/myshop/development/src/assets/womens.jpg" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Women's Wear</h5>                               
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                                                    
+                    </div>       
             </div>
     </div>
     </>
